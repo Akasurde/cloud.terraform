f0b62c2146e18dd36f6c9a6598dde97912d46a5c
Abhijeet Kasurde <akasurde@redhat.com>
2024-01-02 16:31:38 -0800

    Remove superfluous param from get_bin_path
    The parameter `required` in process.get_bin_path is useless.
    Remove and update the usage of get_bin_path in inventory and lookup
    plugins.
    
    Co-authored-by: Felix Fontein <felix@fontein.de>
    Signed-off-by: Abhijeet Kasurde <akasurde@redhat.com>
    Signed-off-by: Abhijeet Kasurde <Akasurde@redhat.com>

--- plugins/inventory/terraform_provider.py
+++ plugins/inventory/terraform_provider.py
@@ -107,6 +107,7 @@ EXAMPLES = r"""
 import os
 from typing import Any, List, Optional
 
+from ansible.errors import AnsibleError
 from ansible.module_utils.common import process
 from ansible_collections.cloud.terraform.plugins.module_utils.errors import TerraformError, TerraformWarning
 from ansible_collections.cloud.terraform.plugins.module_utils.models import (
@@ -191,7 +192,10 @@ class InventoryModule(TerraformInventoryPluginBase):
         if terraform_binary is not None:
             validate_bin_path(terraform_binary)
         else:
-            terraform_binary = process.get_bin_path("terraform", required=True)
+            try:
+                terraform_binary = process.get_bin_path("terraform")
+            except ValueError:
+                raise AnsibleError("Unable to find 'terraform' binary in the path")
 
         # TODO: remove when ansible provider is available
         state_content = []
